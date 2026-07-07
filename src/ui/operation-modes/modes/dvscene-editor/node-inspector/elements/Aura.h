9a1a4c28da8e3299fa9bfd53a745f1ab9d9b730f
Ashrindy <honzula1000@gmail.com>
2026-07-07 12:24:45 +0200

    [DvScene Editor] Updated Aura Element inspector

--- src/ui/operation-modes/modes/dvscene-editor/node-inspector/elements/Aura.h
+++ src/ui/operation-modes/modes/dvscene-editor/node-inspector/elements/Aura.h
@@ -6,13 +6,11 @@ namespace ui::operation_modes::modes::dvscene_editor {
     bool RenderElementInspector<hh::dv::DvNodeElement::ElementID::AURA>(char* element) {
         bool changed = false;
         auto* data = reinterpret_cast<app::dv::DvElementAura::Description*>(element);
-		changed |= CheckboxFlags("Enabled", data->flags, app::dv::DvElementAura::Description::Flags::ENABLED);
-        if(data->flags.test(app::dv::DvElementAura::Description::Flags::ENABLED)){
-            changed |= CheckboxFlags("Curve Enabled", data->flags, app::dv::DvElementAura::Description::Flags::CURVE_ENABLED);
-            changed |= Editor("Node", data->node);
-            if(data->flags.test(app::dv::DvElementAura::Description::Flags::CURVE_ENABLED))
-                changed |= Editor("Finish Node", data->finishNode);
-        }
+		changed |= CheckboxFlags("Use Mesh", data->flags, app::dv::DvElementAura::Description::Flags::USE_MESH);
+        changed |= CheckboxFlags("Curve Enabled", data->flags, app::dv::DvElementAura::Description::Flags::CURVE_ENABLED);
+        changed |= Editor("Node", data->node);
+        if(data->flags.test(app::dv::DvElementAura::Description::Flags::CURVE_ENABLED))
+            changed |= Editor("Finish Node", data->finishNode);
         return changed;
     }
 }
